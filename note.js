2d91ed6da1b106fd105744b9b013983ac09e0ea5
Tanngrisnr <fredrik.wintzell@gmail.com>
2020-11-25 19:43:22 +0100

    added title functionality, started work on errors

--- note.js
+++ note.js
@@ -1,5 +1,6 @@
 const saveBtn = document.getElementById('save');
 const noteList = document.getElementById('note-list')
+const titleInput = document.getElementById('title')
 
 
 let notes = [];
@@ -9,12 +10,42 @@ var quill = new Quill('#editor', {
   });
 
 saveBtn.addEventListener('click', ()=>{
+
+    /* Possible error implementation.
+    if (quill.root.innerHTML = `
+    <p>Hello World!</p>
+    <p>begin a new <strong>note.</strong></p>
+    ` ) {
+      //error¨
+      alert('placeholder error')
+    }
+
+    else if (quill.root.innerHTML = '') {
+      //error
+      alert('empty error')
+    }
+
+    else if (titleInput.value = ''){
+      //error
+      alert('title error')
+    } */
+
+
     const note = {
         data: quill.root.innerHTML,
         id: Date.now(),
+        title: titleInput.value
     }
     notes.push(note);
     addToLocalStorage(notes, noteList);
+    quill.setContents([
+      { insert: 'Hello ' },
+      { insert: 'World! \n'},
+      { insert: 'begin a new ' },
+      {insert: 'note.', attributes: { bold: true }}
+    ]);
+    titleInput.value = ''
+  
 });
 
 function renderNotes(items, container){
@@ -25,7 +56,10 @@ function renderNotes(items, container){
             
             article.setAttribute('class', 'note')
             article.setAttribute('data-key', item.id)
-            article.innerHTML = `${item.data}`
+            article.innerHTML = `
+            <h3 class='note-title'>${item.title}</h3>
+            ${item.data}
+            `
             
            
             console.log(items);
